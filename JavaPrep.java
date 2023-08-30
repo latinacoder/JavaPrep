df5b0900e4ec8eecb7cdc51051326fe92f2d7891
Jess Sepulveda <50001671+latinacoder@users.noreply.github.com>
2023-08-29 21:47:37 -0400

    Create JavaPrep.java

---
+++ JavaPrep.java
@@ -0,0 +1,15 @@
+//class name is the same as the file name, each word capitalized. 
+
+public class HelloYou {
+  public static void main(String[] args) {
+    System.out.println("Hello Jess!");
+    
+  }
+}
+
+
+//main method lists our program tasks
+
+//String[]args is a placeholder for info we want to pass into our program
+
+//System.out.println("Hello World");
