34b2f275e3695f24eadebc509b2a1544f91370b9
Jess Sepulveda <50001671+latinacoder@users.noreply.github.com>
2023-09-08 20:51:21 -0400

    Create CounterLoopInterestRate.java

---
+++ CounterLoopInterestRate.java
@@ -0,0 +1,26 @@
+package Section6;
+
+public class CounterLoop {
+
+    public static void main(String[]args){
+
+        //tester example
+        for (int counter = 1; counter <= 5; counter ++ ){
+            System.out.println(counter);
+        } // close counter for loop
+for (double rate = 2.0; rate <= 5.0; rate++){
+    double interestAmount = calculateInterest(1000,rate);
+    System.out.println("10,000 at " + rate + "% interest = $" + interestAmount);
+} // close for loop
+
+ for ( double rate2 = 7.5; rate2 <= 10.0; rate2 += 0.25 ) {
+     double interestAmount2 = calculateInterest(100, rate2);
+     System.out.println("100 at " + rate2 + "% interest = $" + interestAmount2);
+ };
+    } // close main method
+
+    //new method to calculate interest
+    public static double calculateInterest(double amount, double interestRate){
+        return (amount * (interestRate / 100)); //bc amount comes in as a double (ex: $200 * (5%/100) = 200 * .05 = 10). $10 is the interest accrued
+    }
+} // close class
