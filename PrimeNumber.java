5cddbafa7e31d4aec8fe8515e92f880d91fb9b3f
Jess Sepulveda <50001671+latinacoder@users.noreply.github.com>
2023-09-08 21:32:01 -0400

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -3,14 +3,25 @@ package Section6;
 public class PrimeNumber {
     public static void main (String[]args){
 
-        //test cases:
-        System.out.println("0 is " + (isPrime(0) ? "" : "NOT ") + "a prime number" );
-        System.out.println("1 is " + (isPrime(1) ? "" : "NOT ") + "a prime number" );
-        System.out.println("2 is " + (isPrime(2) ? "" : "NOT ") + "a prime number" );
-        System.out.println("3 is " + (isPrime(3) ? "" : "NOT ") + "a prime number" );
+        // //test cases for isPrime:
+        // System.out.println("0 is " + (isPrime(0) ? "" : "NOT ") + "a prime number" );
+        // System.out.println("1 is " + (isPrime(1) ? "" : "NOT ") + "a prime number" );
+        // System.out.println("2 is " + (isPrime(2) ? "" : "NOT ") + "a prime number" );
+        // System.out.println("3 is " + (isPrime(3) ? "" : "NOT ") + "a prime number" );
 
-        System.out.println("8 is " + (isPrime(8) ? "" : "NOT ") + "a prime number" );
-        System.out.println("17 is " + (isPrime(17) ? "" : "NOT ") + "a prime  number" );
+        // System.out.println("8 is " + (isPrime(8) ? "" : "NOT ") + "a prime number" );
+        // System.out.println("17 is " + (isPrime(17) ? "" : "NOT ") + "a prime  number" );
+
+
+        // new: counter for primenumbers
+           int count = 0;
+
+            for (int i = 10; count < 3 && i <= 50; i++){
+                if (isPrime(i)){
+                    System.out.println("number " + i + " is a prime number");
+                    count++;
+            }
+        }
 
     } // close main method
 
